90cf46eda7d707fb9171c9df7b9e0351c593c341
Tiago <ztiago3@debnetp3.USPIAUMFS>
2018-07-15 18:39:30 -0300

    deixando o código mais limpo e padronizado

--- joke/src/app/joke-list/joke-list.component.ts
+++ joke/src/app/joke-list/joke-list.component.ts
@@ -1,30 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Joke } from '../class/joke';
 
 @Component({
   selector: 'joke-list',
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.css']
 })
-export class JokeListComponent implements OnInit {
+export class JokeListComponent {
 
-  jokes: Object[];
+  jokes: Joke[];
 
   constructor() { 
-    this.jokes = [{
-      setup: "What did the cheese say when it looked int the mirror?",
-      punchline: "Hello-Me (Halloumi)"
-    },
-    {
-      setup: "What kind of cheese say when it looked int the mirror?",
-      punchline: "Mask a pony"
-    },
-    {
-      setup: "A kid",
-      punchline: "I thought"
-    }]
+    // this.jokes = [{
+    //   setup: "What did the cheese say when it looked int the mirror?",
+    //   punchline: "Hello-Me (Halloumi)",
+    //   hide:true
+    // },
+    // {
+    //   setup: "What kind of cheese say when it looked int the mirror?",
+    //   punchline: "Mask a pony",
+    //   hide:true
+    // },
+    // {
+    //   setup: "A kid",
+    //   punchline: "I thought",
+    //   hide:true
+    // }]
+
+    this.jokes = [
+      new Joke("What did the cheese say when it looked int the mirror?",
+        "Hello-Me (Halloumi)"),
+        new Joke("What kind of cheese say when it looked int the mirror?",
+        "Mask a pony"),
+        new Joke("A kid",
+        "I thought"),
+    ]
   }
 
-  ngOnInit() {
-  }
+  // toggle(joke){
+  //   joke.hide = !joke.hide;
+  // }
+
 
 }
--- joke/src/app/joke/joke.component.ts
+++ joke/src/app/joke/joke.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Joke } from '../class/joke';
 
 @Component({
   selector: 'joke',
   templateUrl: './joke.component.html',
   styleUrls: ['./joke.component.css']
 })
-export class JokeComponent implements OnInit {
+// export class JokeComponent implements OnInit {
+//   joke: Joke;
+//   setup: string;
+//   punchline: string;
 
-  setup: string;
-  punchline: string;
+//   constructor() { 
+//     this.setup = "What did te cheese say when it looked in the mirror?";
+//     this.punchline = "Halloumi (Hello Me)";
+//   }
 
-  constructor() { 
-    this.setup = "What did te cheese say when it looked in the mirror?";
-    this.punchline = "Halloumi (Hello Me)";
-  }
+//   ngOnInit() {
+//   }
 
-  ngOnInit() {
-  }
+// }
 
+export class JokeComponent{  
+  @Input('joke') data: Joke;
 }
