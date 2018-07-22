fb91e31636362b2e5f0e1189866c86b217fc5b4d
Tiago <ztiago3@debnetp3.USPIAUMFS>
2018-07-22 18:34:07 -0300

    childview

--- joke/src/app/app.component.ts
+++ joke/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Joke } from './class/joke';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  joke: Joke = new Joke("teste novo", "teste detalhe");
 }
--- joke/src/app/joke-list/joke-list.component.ts
+++ joke/src/app/joke-list/joke-list.component.ts
@@ -1,25 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { Joke } from '../class/joke';
+import { JokeComponent } from '../joke/joke.component';
 
 @Component({
   selector: 'joke-list',
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.css']
 })
-export class JokeListComponent {
+export class JokeListComponent implements AfterViewInit {
 
-  jokes: Joke[];
+  jokes: Joke[] = [
+    new Joke("What did the cheese say when it looked int the mirror?",
+      "Hello-Me (Halloumi)"),
+      new Joke("What kind of cheese say when it looked int the mirror?",
+      "Mask a pony")
+  ];
 
-  constructor() { 
-    this.jokes = [
-      new Joke("What did the cheese say when it looked int the mirror?",
-        "Hello-Me (Halloumi)"),
-        new Joke("What kind of cheese say when it looked int the mirror?",
-        "Mask a pony"),
-        new Joke("A kid",
-        "I thought")
-    ]
+  @ViewChild(JokeComponent) jokeViewChild: JokeComponent;
+  @ViewChildren(JokeComponent) jokeViewChildren: QueryList<JokeComponent>;
+
+  constructor(){
+    console.log(`new - jokeViewChild is ${this.jokeViewChild}`);
   }
+
+  ngAfterViewInit(){
+    console.log(`ngAfterViewInit - jokeViewChild is ${this.jokeViewChild}`);
+    let jokes: JokeComponent[] = this.jokeViewChildren.toArray();
+    console.log(jokes);
+  }
+
+  // constructor() { 
+  //   this.jokes = [
+  //     new Joke("What did the cheese say when it looked int the mirror?",
+  //       "Hello-Me (Halloumi)"),
+  //       new Joke("What kind of cheese say when it looked int the mirror?",
+  //       "Mask a pony"),
+  //       new Joke("A kid",
+  //       "I thought")
+  //   ]
+  // }
 
   // addJoke(joke){
   //   this.jokes.unshift(joke);
